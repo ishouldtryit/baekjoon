f875a7a855f8ef7519c6224867c595c98a76c7d4
ishouldtryit <ishouldtryit@naver.com>
2023-05-01 13:20:12 +0900

    백준 5단계 완료

---
+++ baekjoon/src/beakjoonStep005/no_11_11718.java
@@ -0,0 +1,29 @@
+package beakjoonStep005;
+	
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+	
+	public class no_11_11718 {
+	
+		public static void main(String[] args) throws IOException {
+			
+	
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+			String input = "";
+			
+			while((input=br.readLine())!=null) {
+				bw.write(input);
+				bw.newLine();
+				
+			}
+		
+			bw.flush();
+			bw.close();
+			br.close();
+		}
+	}
