1e36ba0ddc06644dc81f094b5c25f8a5b84d95d4
ishouldtryit@naver.com <ishouldtryit@naver.com>
2023-05-09 00:10:50 +0900

    백준 6단계 진행중

--- baekjoon/src/beakjoonStep006/no_06_1157.java
+++ baekjoon/src/beakjoonStep006/no_06_1157.java
@@ -5,10 +5,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 	
 	public class no_06_1157{
 	
@@ -19,14 +17,31 @@ import java.util.Set;
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 			String input = br.readLine().toUpperCase();
-			char[] c = new char[input.length()];
+			Map<Character, Integer> map = new HashMap<>();
 			for(int a=0; a<input.length(); a++) {
-				c[a] = input.charAt(a);
+				if(map.containsKey(input.charAt(a))) {
+					map.put(input.charAt(a), map.get(input.charAt(a))+1);
+				}else {
+					map.put(input.charAt(a), 1);
+				}
 			}
+			int max=0;
+			char c = 0;
+			int count =0;
 			
+			for(Map.Entry<Character, Integer> entry : map.entrySet() ) {
+				if(entry.getValue()>max) {
+					max=entry.getValue();
+					c = entry.getKey();
+					count = 1;
+				}else if(entry.getValue()==max) {
+					count++;
+				}
+			} 
 			
+			String result = (count > 1) ? "?" : String.valueOf(c);
 			
-			
+			bw.write(result);
 			bw.flush();
 			bw.close();
 			br.close();
